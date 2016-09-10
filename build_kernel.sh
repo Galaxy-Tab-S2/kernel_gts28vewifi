d3f5ddbce5540e695b5a1a625e5f4fc2962d718d
CTXz <ctx.xda@gmail.com>
2016-09-11 00:46:46 +0200

    make: Improve build_kernel.sh
    NOTE: Toolchains must be defined manually
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/ctxz/android/toolchain/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
-make VARIANT_DEFCONFIG=msm8976_sec_gts28vewifi_eur_defconfig msm8976_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DE
-make -j
+make clean && make mrproper
+make VARIANT_DEFCONFIG=msm8976_sec_gts28vewifi_eur_defconfig msm8976_sec_defconfig
+make menuconfig
+make -j4
