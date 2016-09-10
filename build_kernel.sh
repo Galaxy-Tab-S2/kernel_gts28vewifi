09c9a3f2041f8328993965bf26a4470b059979d8
Lightn1ng <ryane512@gmail.com>
2016-09-10 18:52:57 -0400

    Use toolchain path per user
    Ryan El Kochta, ryane512@gmail.com

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/ctxz/android/toolchain/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/$(whoami)/android/toolchain/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 make clean && make mrproper
 make VARIANT_DEFCONFIG=msm8976_sec_gts28vewifi_eur_defconfig msm8976_sec_defconfig
